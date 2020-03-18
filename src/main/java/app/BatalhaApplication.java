0fb27f78aec46d3ccb88882a7157efe8eb60e6eb
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-03-17 23:16:25 -0300

    Criação da classe principal do sistema para inicializar o jar

---
+++ src/main/java/app/BatalhaApplication.java
@@ -0,0 +1,12 @@
+package app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"app","database"})
+public class BatalhaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatalhaApplication.class, args);
+    }
+}
