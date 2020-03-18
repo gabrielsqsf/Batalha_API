c1946ddc3b03d7ebbb96adbc5a7475db5aaa2b23
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-03-17 23:32:30 -0300

    Modificando o controller para suportar a criação dos DAOs/Objetos de Lógica na criação do controller, além de melhorar a implementação de alguns pontos.

--- src/main/java/app/BatalhaController.java
+++ src/main/java/app/BatalhaController.java
@@ -1,5 +1,6 @@
-package batalha.controller;
+package app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,21 +11,28 @@ import org.springframework.web.server.ResponseStatusException;
 
 import batalha.LogicaBatalha;
 import batalha.LogicaTurno;
+import batalha.persistencia.BatalhaDAO;
 import batalha.persistencia.BatalhaInexistenteException;
+import batalha.persistencia.ErroPersistenciaBatalha;
 import batalha.response.BatalhaResponse;
 import batalha.response.ListaBatalhaResponse;
 import batalha.response.LogicaBatalhaWrapper;
 import batalha.response.LogicaTurnoWrapper;
 import batalha.response.TurnoResponse;
+import personagem.persistencia.PersonagemDAO;
 import personagem.persistencia.PersonagemInexistenteException;
 import personagem.persistencia.SemMonstrosException;
 
 @RestController
 public class BatalhaController {
-	
+	private LogicaBatalha logBatalha;
+	public BatalhaController(BatalhaDAO dao, PersonagemDAO personagemDAO) {
+		logBatalha = new LogicaBatalha(dao, personagemDAO);
+	}
 	@GetMapping("/batalha")
 	public ListaBatalhaResponse  listaBatalhas() {
-		ListaBatalhaResponse resp = LogicaBatalhaWrapper.listaBatalhas();
+		LogicaBatalhaWrapper logWrapper = new LogicaBatalhaWrapper(logBatalha);
+		ListaBatalhaResponse resp = logWrapper.listaBatalhas();
 		return resp; 
 	}
 	
@@ -32,12 +40,15 @@ public class BatalhaController {
 	public BatalhaResponse criaBatalha(@RequestParam(name="nickname") String nickname, 
 			@RequestParam(name="personagem") String personagem) {
 		BatalhaResponse battle = null;
+		LogicaBatalhaWrapper logWrapper = new LogicaBatalhaWrapper(logBatalha);
 		try {
-			battle = LogicaBatalhaWrapper.criarBatalha(nickname, personagem);
+			battle = logWrapper.criarBatalha(nickname, personagem);
 		} catch(PersonagemInexistenteException e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "personagem inexistente!");
 		} catch(SemMonstrosException e) {
 			throw new ResponseStatusException(HttpStatus.EXPECTATION_FAILED, "não foram encontrados monstros na base!");
+		} catch (ErroPersistenciaBatalha e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "não foi possivel salvar a batalha!");
 		}
 		return battle;
 	}
@@ -45,8 +56,9 @@ public class BatalhaController {
 	@GetMapping("/batalha/{id}")
 	public BatalhaResponse  listaBatalhasId(@RequestParam(name="id") long id) {
 		BatalhaResponse resp = null;
+		LogicaBatalhaWrapper logWrapper = new LogicaBatalhaWrapper(logBatalha);
 		try {
-			LogicaBatalha.getBatalha(id);
+			logWrapper.getBatalha(id);
 		} catch (BatalhaInexistenteException e) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Essa batalha não foi encontrada!");
 		}
@@ -56,8 +68,9 @@ public class BatalhaController {
 	@PostMapping("/batalha/{id}")
 	public TurnoResponse CriarTurno(@RequestParam(name="id") long id) {
 		TurnoResponse turno = null;
+		LogicaTurnoWrapper logWrapper = new LogicaTurnoWrapper(logBatalha);
 		try {
-			turno = LogicaTurnoWrapper.criarTurno(id);
+			turno = logWrapper.criarTurno(id);
 		} catch (Exception e) {
 			
 		}
