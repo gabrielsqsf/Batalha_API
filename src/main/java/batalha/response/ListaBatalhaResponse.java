3f671ae508cc9fa22536dc246f06042b4115bb6b
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-03-20 01:55:59 -0300

    Ajuste de atributos

--- src/main/java/batalha/response/ListaBatalhaResponse.java
+++ src/main/java/batalha/response/ListaBatalhaResponse.java
@@ -3,14 +3,7 @@ package batalha.response;
 import java.util.List;
 
 public class ListaBatalhaResponse {
-	private String status;
 	private List<BatalhaResponse> batalhas;
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
 	public List<BatalhaResponse> getBatalhas() {
 		return batalhas;
 	}
